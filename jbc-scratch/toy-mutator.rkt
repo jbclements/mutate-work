df7bf01b2c56f7ee084daab716033a0ccd5fb1e3
John Clements <clements@racket-lang.org>
2024-07-11 18:23:35 -0700

    scratch file

---
+++ jbc-scratch/toy-mutator.rkt
@@ -0,0 +1,30 @@
+#lang at-exp racket
+
+;; just messing around with mutation
+
+(require mutate
+         syntax/parse
+         racket/stream)
+ 
+(define program-mutations
+  (build-mutation-engine
+   #:mutators
+   (define-simple-mutator (permute-cond stx)
+     #:pattern ({~datum cond} [tests argses ...] ...)
+     (for/stream ([args (in-list (attribute argses))])
+       #`(begin #,@args)))
+   #:syntax-only
+   #:streaming
+   #:module-mutator))
+ 
+(define program-to-mutate
+  #'(module test-program racket
+      (#%module-begin
+       (require "a.rkt")
+       (cond [(try1) 'answer1]
+             [(try2) 'answer2 'answer2andahalf]
+             [(try3) 'answer3])
+       (displayln y))))
+
+(map syntax->datum (stream->list (program-mutations program-to-mutate)))
+
